6ce08913d0972ec88aad408dff46949af32bd8bf
adityaanantjadhav26@gmail.com <115895553+adityaanantjadhav@users.noreply.github.com>
2023-10-08 11:58:30 +0530

    Updated the N Queen Problem

--- daa3NQueen.cpp
+++ daa3NQueen.cpp
@@ -1,7 +1,8 @@
+
+//TIB 26 Aditya Anant Jadhav
+
 #include<iostream>
 using namespace std;
-
-
 
 int ans(int n,int arr[2][10],int q){     
 	int i;
@@ -38,7 +39,7 @@ int ans(int n,int arr[2][10],int q){
 		if(i==n){
 			arr[1][q]=-1;
 			q--;
-			//ans(n,arr,q);
+			cout<<"\nlets back track::\n";
 			return q;
 		}
 		else{
@@ -59,11 +60,6 @@ int main(){
 		arr[0][l]=0;
 		arr[1][l]=0;
 	}
-
-    arr[1][0]=3;
-    arr[1][1]=5;
-
-
 	while(q!=n){
         cout<<"x::";
         for(int i=0;i<n;i++){
@@ -97,3 +93,72 @@ int main(){
         }cout<<"|\n";
     }
 }
+
+/*		OUTPUT
+
+enter the no. of queen::4
+x::0    0       0       0
+y::0    0       0       0
+cannot place queen1 at (1,1)
+queen1 can be placed at (1,2)
+x::0    1       0       0
+y::0    2       0       0
+cannot place queen2 at (2,1)
+cannot place queen2 at (2,2)
+cannot place queen2 at (2,3)
+
+lets back track::
+x::0    1       0       0
+y::0    2       -1      0
+queen1 can be placed at (1,3)
+x::0    1       0       0
+y::0    3       -1      0
+cannot place queen2 at (2,0)
+queen2 can be placed at (2,1)
+x::0    1       2       0
+y::0    3       1       0
+cannot place queen3 at (3,1)
+cannot place queen3 at (3,2)
+cannot place queen3 at (3,3)
+
+lets back track::
+x::0    1       2       0
+y::0    3       1       -1
+cannot place queen2 at (2,2)
+cannot place queen2 at (2,3)
+
+lets back track::
+x::0    1       2       0
+y::0    3       -1      -1
+
+lets back track::
+x::0    1       2       0
+y::0    -1      -1      -1
+queen0 placed at (0,1)
+x::0    1       2       0
+y::1    -1      -1      -1
+cannot place queen1 at (1,0)
+cannot place queen1 at (1,1)
+cannot place queen1 at (1,2)
+queen1 can be placed at (1,3)
+x::0    1       2       0
+y::1    3       -1      -1
+queen2 can be placed at (2,0)
+x::0    1       2       0
+y::1    3       0       -1
+cannot place queen3 at (3,0)
+cannot place queen3 at (3,1)
+queen3 can be placed at (3,2)
+row column
+0   1
+1   3
+2   0
+3   2
+
+  0 1 2 3
+0| |@| | |
+1| | | |@|
+2|@| | | |
+3| | |@| |
+
+*/
